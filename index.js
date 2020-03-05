d67e6e2694a29a8d2971965d00862f1e40c4ff6e
danielius19 <danielius.stirbys19@my.northampton.ac.uk>
2020-03-05 15:44:12 +0000

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+/*Todo list*/
+window.onload = function (){
+
+}
+//variables
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
+let btnClr = document.getElementById("btnClr");
+let id = 1;
+//listItem = {item: "todo item", checked: false}
+let liItem = "";
+let todoList = [];
